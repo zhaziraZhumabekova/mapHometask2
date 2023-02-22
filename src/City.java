7a9fb568d8f9a872c088c4b2a3aa42de90287b53
Toshiba <zhazira9818@gmail.com>
2023-02-22 21:55:21 +0600

    map

--- src/City.java
+++ src/City.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class City {
+public class City {
+    private int code;
+    private String name;
+
+    public City(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "City{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<City> arrayList = new ArrayList<>();
+
+        City city = new City(82321, "Moscow");
+        City city2 = new City(13564, "Bishkek");
+        City city3 = new City(45987, "Osh");
+        City city4 = new City(65786, "Istanbul");
+
+        arrayList.add(city);
+        arrayList.add(city2);
+        arrayList.add(city3);
+        arrayList.add(city4);
+
+        for (City cityName : arrayList) {
+            if (cityName.getCode() % 2 != 0) {
+                Set<City> ts = new TreeSet<>((o1, o2) -> o2.getCode() - o1.getCode());
+                ts.add(cityName);
+                System.out.println(ts);
+            }
+        }
+        System.out.println(arrayList);
+
     }
 }
